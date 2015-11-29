4a0d2192e54079d162b9f64dd4e4509124517ae4
marie <marie.jones@etudiant.univ-lille1.fr>
2015-11-29 11:57:48 +0100

    rm des

--- des/examples/SQLDebugger/e4trust.sql
+++
@@ -1,6 +0,0 @@
--- SQL Debugger
--- November 2011
--- Example 4
---
--- Trust file
-create view u(a) as select * from s where a>=1 or a>0;
--- des/examples/ddb.pl
+++
@@ -1,7 +0,0 @@
-:-type(p(a:int)).
-:-type(q(b:int)).
-:-type(r(a:int,b:int,c:string)).
-:-pk(p,[a]).
-:-pk(q,[b]).
-:-fk(r,[a],p,[a]).
-:-fk(r,[b],q,[b]).
